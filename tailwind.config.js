0c346443383761f3a38e8680606e4eeba8430048
Anthony Oyathelemhi <tonero91@gmail.com>
2022-06-24 07:55:17 +0100

    implement layout for desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts, tsx}'],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#005B96',
+      },
+      textColor: {
+        dark: '#011F4B',
+        light: '#7E8299',
+      },
+    },
   },
   plugins: [],
 };
